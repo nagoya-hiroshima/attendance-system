aee4e86794a994b6082bdbbd0d7ac08e3c5881b1
takahashi1182 <takahasu0709@gmail.com>
2025-12-09 14:36:11 +0900

    個人フォルダを更新

--- 髙橋/v2/attendance/src/main/java/attendance/system/attendance/repository/DeployRepository.java
+++ 髙橋/v2/attendance/src/main/java/attendance/system/attendance/repository/DeployRepository.java
@@ -1,5 +1,9 @@
 package attendance.system.attendance.repository;
 
-public class DeployRepositoey {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import attendance.system.attendance.model.Deploy;
+
+@Repository
+public interface DeployRepository extends JpaRepository<Deploy, Integer> {
 }
