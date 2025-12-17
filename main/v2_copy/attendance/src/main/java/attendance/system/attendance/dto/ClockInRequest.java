500fee403538ec2d714ac8b5c98de437b5e0d724
takahashi1182 <takahasu0709@gmail.com>
2025-12-17 16:18:54 +0900

    AttendanceService関連

--- main/v2_copy/attendance/src/main/java/attendance/system/attendance/dto/ClockInRequest.java
+++ main/v2_copy/attendance/src/main/java/attendance/system/attendance/dto/ClockInRequest.java
@@ -1,5 +1,20 @@
 package attendance.system.attendance.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 public class ClockInRequest {
-    
+
+    @NotNull
+    private Long userId;
+
+    public ClockInRequest() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
