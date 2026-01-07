215045284b72c35695df02894cea9461d75b1d0b
ritsu8 <ponchan.0801@icloud.com>
2026-01-07 16:00:05 +0900

    work_placeをt_kintaiでちゃんと更新できました！

--- 前野/v3_copy/attendance/src/main/java/attendance/system/attendance/repository/AttendanceRepository.java
+++ 前野/v3_copy/attendance/src/main/java/attendance/system/attendance/repository/AttendanceRepository.java
@@ -2,7 +2,6 @@ package attendance.system.attendance.repository;
 
 import attendance.system.attendance.model.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
